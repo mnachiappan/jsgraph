ba410b745ab34c39872fc81d55575832542b0547
Meyyappan Nachiappan <mnachiappan1995@gmail.com>
2014-07-18 22:57:59 -0800

    Add data function and tests.

--- js/script.js
+++ js/script.js
@@ -71,9 +71,101 @@ function basicChartData(){
 function lineChartData(){
     this.labels = [];
     this.datasets = [];
+    var _labelTag = "label";
+    var _elemTag = "elem";
+
+    var createDataElement = function(data){
+        var result = {};
+        result[_elemTag] = data;
+        return result;
+    }
+
+
+    this.addData = function(label, fillColor, strokeColor, pointColor, pointStrokeColor, pointHighlightFill, pointHighlightStroke, data){
+        fillColor = this.standardizeColor(fillColor);
+        strokeColor = this.standardizeColor(strokeColor);
+        pointColor = this.standardizeColor(pointColor);
+        pointHighlightStroke = this.standardizeColor(pointHighlightStroke);
+
+        var localData = []
+        for(var i = 0; i < data.length; i++){
+            localData.push(createDataElement(data[i]));
+        }
+
+
+        var result = {
+            "label": label,
+            "fillColor": fillColor,
+            "strokeColor": strokeColor,
+            "pointColor": pointColor,
+            "pointStrokeColor": pointStrokeColor,
+            "pointHighlightFill": pointHighlightFill,
+            "pointHighlightStroke": pointHighlightStroke,
+            "data": localData
+        };
+        this.datasets.push(result);
+    }
+
+    this.getDatasets = function(){
+        return this.datasets;
+    }
+
+    this.getData = function(index){
+        if(index >= this.datasets.length){
+            return null;
+        }else{
+            return this.datasets[index];
+        }
+    }
+
+    this.setDataFillColor = function(index, fillColor){
+        fillColor = this.standardizeColor(fillColor);
+        this.datasets[index].fillColor = fillColor;
+    }
+
+    this.setDataStrokeColor = function(index, strokeColor){
+        strokeColor = this.standardizeColor(strokeColor);
+        this.datasets[index].strokeColor = strokeColor;
+    }
+
+    this.setDataPointColor = function(index, pointColor){
+        pointColor = this.standardizeColor(pointColor);
+        this.datasets[index].pointColor = pointColor;
+    }
+
+    this.setDataPointStrokeColor = function(index, pointStrokeColor){
+        this.datasets[index].pointStrokeColor = pointStrokeColor;
+    }
+
+    this.setDataPointHighlightFill = function(index, pointHighlightFill){
+        pointHighlightFill = this.standardizeColor(pointHighlightFill);
+        this.datasets[index].pointHighlightFill = pointHighlightFill;
+    }
+
+
 }
 
-lineChartData.prototype = new basicChartData();
+lineChartData.prototype = (new basicChartData());
+/*
+    'color' is of form {"r": 10, "b": 20, "g": 30, "o": 0.4}
+ */
+lineChartData.prototype.standardizeColor = function(color){
+    if(color["r"] > 255){
+        color["r"] = 255;
+    }
+    if(color["b"] > 255){
+        color["b"] = 255;
+    }
+    if(color["g"] > 255){
+        color["g"] = 255;
+    }
+    if(color["o"] > 1){
+        color["o"] = 1;
+    }
+    return color;
+}
+
+
 var graphApp = angular.module('graphApp', []);
 
 graphApp.factory('DataFactoryMany', function () {
