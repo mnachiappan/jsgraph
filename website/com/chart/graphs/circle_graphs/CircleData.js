5200848946dfaef46bb574a0aa3e88cc8d3e34ad
Meyyappan Nachiappan <mnachiappan1995@gmail.com>
2014-08-09 13:07:36 -0800

    building data object for circle graphs

---
+++ website/com/chart/graphs/circle_graphs/CircleData.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Meyyappan on 8/9/2014.
+ */
+var CircleData = (function(){
+    /*
+        datasets = [
+            {
+                "value": 3,
+                "color": {r: 218, g: 165, b: 32, o: 1},
+                "highlight": {r: 218, g: 165, b: 32, o: 0.8},
+                "label": "label"
+            },...
+        ]
+     */
+    function CircleData(){
+        this.datasets = [];
+        this.ColorGenerator = new ColorGenerator();
+    }
+
+    CircleData.prototype.addEmptyDataSet
+
+
+    return CircleData;
+})();
